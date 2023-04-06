5352e11c18e5a5cc2e46784465ee28249a259bea
Mohamed <mbrahimi93@gmail.com>
2023-04-06 15:10:13 +0200

    Final Commit :mag:

---
+++ WindowsFormsAppGM13/FormGenerale.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace WindowsFormsAppGM13
+{
+    partial class FormGenerale
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonClient = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonClient
+            // 
+            this.buttonClient.Font = new System.Drawing.Font("PMingLiU-ExtB", 32.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClient.Location = new System.Drawing.Point(37, 39);
+            this.buttonClient.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonClient.Name = "buttonClient";
+            this.buttonClient.Size = new System.Drawing.Size(356, 176);
+            this.buttonClient.TabIndex = 0;
+            this.buttonClient.Text = "Clients";
+            this.buttonClient.UseVisualStyleBackColor = true;
+            this.buttonClient.Click += new System.EventHandler(this.buttonClient_Click);
+            // 
+            // FormGenerale
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.buttonClient);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FormGenerale";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gestion Matos 13";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClient;
+    }
+}
+
---
+++ WindowsFormsAppGM13/FormGenerale.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppGM13
+{
+    public partial class FormGenerale : Form
+    {
+        public FormGenerale()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClient_Click(object sender, EventArgs e)
+        {
+            FormClient dlg = new FormClient();
+            dlg.ShowDialog();
+
+
+
+
+        }
+
+    }
+}
